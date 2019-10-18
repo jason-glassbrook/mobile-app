94b8dabbf4b10e62b13d3c54785e77edd97ef35b
Bryan Szendel <bryanszendel@Bryans-MacBook-Pro.local>
2019-10-18 08:59:53 -0600

    WIP: added Expo LocalAuthentication for touch/face ID authentication

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { store } from './src/store/store';
 import * as Font from 'expo-font';
 import constants from './src/helpers/constants';
 import Navigator from './src/navigation';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default class App extends Component {
   state = { fontLoaded: false };
@@ -17,6 +18,37 @@ export default class App extends Component {
   };
 
   async componentDidMount() {
+    await LocalAuthentication.hasHardwareAsync()
+      .then(res => 
+        console.log(res),
+        await LocalAuthentication.supportedAuthenticationTypesAsync()
+          .then(res => 
+            console.log(res),
+            await LocalAuthentication.isEnrolledAsync()
+              .then(res => 
+                console.log(res),
+                await LocalAuthentication.authenticateAsync({
+                  promptMessage: 'Use Touch/Face ID to access the app', 
+                  fallbackLabel: 'Use Passcode'
+                })
+                .then(res => 
+                  console.log(res)
+                )
+                .catch(err => 
+                  console.log(err)
+                )
+              )
+              .catch(err => 
+                console.log(err)
+              )
+          )
+          .catch(err => 
+            console.log(err)
+          )
+      )
+      .catch(err => console.log(err));
+
+
     await Font.loadAsync({
       [constants.fontFamily]: require('./assets/fonts/Futura-Light.otf'),
       ['Roboto_medium']: require('./assets/fonts/Roboto_medium.otf')
