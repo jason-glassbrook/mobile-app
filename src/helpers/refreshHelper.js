cbadbb54d866d64fc344afed271757e3844e7bf3
Bryan Szendel <bryan.szendel@gmail.com>
2019-10-22 17:05:41 -0600

    removed React

--- src/helpers/refreshHelper.js
+++ src/helpers/refreshHelper.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store'
 import getEnvVars from '../../environment.js';
@@ -16,7 +15,7 @@ const refreshHelper = async () => {
     grant_type: 'authorization_code',
     code: code,
   }
-  
+
   axios
     .post('https://connectourkids.auth0.com/oauth/token', refreshParams)
     .then(res => {
