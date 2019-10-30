84bc637cf236e757f3a3343f593e13059d87faaf
Bryan Szendel <bryan.szendel@gmail.com>
2019-10-30 13:15:14 -0600

    changed color of headerTitle and logos
    Co-authored-by: Tania Keller <tanislavens@yahoo.com>
    Co-authored-by: Bishop Lake <lake.d.bishop@gmail.com>

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -8,13 +8,14 @@ import {
   TouchableWithoutFeedback
 } from 'react-native';
 import logoImg from '../../assets/simple-logo.png';
+import { Ionicons } from '@expo/vector-icons'
 import constants from '../helpers/constants';
 import { sendEvent } from '../helpers/createEvent';
 import { connect } from 'react-redux';
 
 const HeaderTitle = ({ title, navigation, email }) => (
   // title should be the string of the components name
-  <View style={{ flexDirection: 'row' }}>
+  <View style={{ flexDirection: 'row', justifyContent: 'center', width: '100%' }}>
     {/* on android the text renders left aligned and therefore we put the logo next to it, ios renders centered */}
     {Platform.OS === 'android' ? (
       <TouchableWithoutFeedback
@@ -30,15 +31,16 @@ const HeaderTitle = ({ title, navigation, email }) => (
         />
       </TouchableWithoutFeedback>
     ) : null}
-    <Text style={styles.text}>{title}</Text> 
+        <Text style={styles.text}>{title}</Text> 
+        
   </View>
 );
 
 const styles = StyleSheet.create({
   text: {
-    color: constants.highlightColor,
-    fontSize: 20,
-    fontFamily: constants.fontFamily
+    color: 'white',
+    fontSize: 25,
+    fontFamily: constants.headerFont
   }
 });
 
