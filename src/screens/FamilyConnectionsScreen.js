f2363477d0e8059f9f223097610f5c46f96d9923
Bryan Szendel <bryan.szendel@gmail.com>
2019-11-08 09:59:47 -0700

    removed 200 lines of commented code

--- src/screens/FamilyConnectionsScreen.js
+++ src/screens/FamilyConnectionsScreen.js
@@ -126,31 +126,6 @@ class FamilyConnectionsScreen extends Component {
       }
     }
 
-    // ------AGE FILTER functionality------
-    // if (!this.state.filters.zero_five && !this.state.filters.six_nine && !this.state.filters.ten_thirteen && !this.state.filters.fourteen_eighteen) { //if nothing is selected -- do nothing
-    // } else {
-    //  let year = new Date()
-    //  let noNullBirthday = []
-    //  let nullBirthday = []
-    //  for (c in filteredCases) { //pull out the object with a null value for birthday so it doesnt break the if statements starting on line 122
-    //    if (filteredCases[c].birthday === null) {
-    //      nullBirthday.push(filteredCases[c])
-    //    } else {
-    //      noNullBirthday.push(filteredCases[c])
-    //    }
-    //  }
-    //  if (!this.state.filters.zero_five) {noNullBirthday = noNullBirthday.filter(c => (year.getFullYear() - c.birthday.slice(0,4)) > 5)} //if this is not selected -- dont return ages 0-5
-    //  if (!this.state.filters.six_nine) {noNullBirthday = noNullBirthday.filter(c => !((year.getFullYear() - c.birthday.slice(0,4) >= 6) && (year.getFullYear() - c.birthday.slice(0,4)) <= 9 ))}
-    //  if (!this.state.filters.ten_thirteen) {noNullBirthday = noNullBirthday.filter(c => !((year.getFullYear() - c.birthday.slice(0,4) >= 10) && (year.getFullYear() - c.birthday.slice(0,4)) <= 13))}
-    //  if (!this.state.filters.fourteen_eighteen) {noNullBirthday = noNullBirthday.filter(c => !((year.getFullYear() - c.birthday.slice(0,4) >= 14) && (year.getFullYear() - c.birthday.slice(0,4)) <= 18))}
-    //  noNullBirthday = noNullBirthday.filter(c => (year.getFullYear() - c.birthday.slice(0,4) < 18))
-    //  if (!this.state.filters.unspecified) { //add null birthdays back if unspecified is checked
-    //   filteredCases = noNullBirthday
-    // } else {
-    //   filteredCases = noNullBirthday.concat(nullBirthday)
-    //   }
-    // }
-
     // ------SORTING Functionality------
     const name = (a, b) => {
       const A = a.full_name.toUpperCase();
@@ -175,19 +150,6 @@ class FamilyConnectionsScreen extends Component {
       }
       return comparison;
     };
-
-    //leaving this out for now because year and day are not required
-    // const DOB = (a, b) => {
-    //   const A = a.birthday;
-    //   const B = b.birthday;
-    //   let comparison = 0;
-    //   if (A < B) {
-    //     comparison = 1;
-    //   } else {
-    //     comparison = -1;
-    //   }
-    //   return comparison;
-    // }
 
     const created = (a, b) => {
       const A = a.created_at;
@@ -215,25 +177,7 @@ class FamilyConnectionsScreen extends Component {
 
     if (this.state.filters.name) {
       filteredCases.sort(lastName);
-    }
-
-    //leaving this out for now because year and day are no longer required
-    // *******BIRTHDAY FILTER*********
-    // else if (this.state.filters.DOB) {
-    //   let Birthdays = []
-    //   let noBirthdays = []
-    //   for (c in filteredCases) { //pull out the object with a null value for birthday so it doesnt break
-    //     if (filteredCases[c].birthday === null) {
-    //       noBirthdays.push(filteredCases[c])
-    //     } else {
-    //       Birthdays.push(filteredCases[c])
-    //     }
-    //     Birthdays.sort(DOB)
-    //   }
-    //   filteredCases = Birthdays.concat(noBirthdays)
-    // }
-
-    else if (this.state.filters.created) {
+    } else if (this.state.filters.created) {
       filteredCases.sort(created);
     } else if (this.state.filters.updated) {
       filteredCases.sort(updated);
@@ -250,15 +194,6 @@ class FamilyConnectionsScreen extends Component {
     const fullYear = new Date();
     return (
       <SafeAreaView>
-        {/* ADDCASE BUTTON -  leaving out for now */}
-        {/* <Button
-          title="Add Case"
-          buttonStyle={{ backgroundColor: constants.highlightColor }}
-          containerStyle={styles.addCaseButton}
-          onPress={() => {
-            this.setState({ addCaseModalVisible: true });
-          }}
-        /> */}
         <View style={{ flexDirection: "column", alignItems: 'flex-start', justifyContent: 'flex-start' }}>
         <SearchBar
             inputStyle={{fontSize: 16}}
@@ -372,40 +307,6 @@ class FamilyConnectionsScreen extends Component {
                 style={{ height: 1, backgroundColor: "lightgray", margin: 20 }}
               />
 
-                {/* AGE FILTER */}
-              {/* <Text style={{ fontSize: 20, fontWeight: "800", textAlign: "center" }}>
-              Age Range
-            </Text>
-            <CheckBox
-              containerStyle={{ backgroundColor: "white", borderColor: "white" }}
-              title='0 - 5 years'
-              size={16}
-              checked={this.state.filters.zero_five}
-              onPress={() => this.setState({...this.state, filters: {...this.state.filters, zero_five: !this.state.filters.zero_five}})}
-            />
-            <CheckBox
-              containerStyle={{ backgroundColor: "white", borderColor: "white" }}
-              title='6 - 9 years'
-              size={16}
-              checked={this.state.filters.six_nine}
-              onPress={() => this.setState({...this.state, filters: {...this.state.filters, six_nine: !this.state.filters.six_nine}})}
-            />
-            <CheckBox
-              containerStyle={{ backgroundColor: "white", borderColor: "white" }}
-              title='10 - 13 years'
-              size={16}
-              checked={this.state.filters.ten_thirteen}
-              onPress={() => this.setState({...this.state, filters: {...this.state.filters, ten_thirteen: !this.state.filters.ten_thirteen}})}
-            />
-            <CheckBox
-              containerStyle={{ backgroundColor: "white", borderColor: "white" }}
-              title='14 - 18 years'
-              size={16}
-              checked={this.state.filters.fourteen_eighteen}
-              onPress={() => this.setState({...this.state, filters: {...this.state.filters, fourteen_eighteen: !this.state.filters.fourteen_eighteen}})}
-            />
-            <Divider style={{ height: 1, backgroundColor: 'lightgray', margin: 20 }} /> */}
-
               <Text
                 style={{ fontSize: 20, fontWeight: "800", textAlign: "center" }}
               >
@@ -433,23 +334,6 @@ class FamilyConnectionsScreen extends Component {
                   })
                 }
               />
-
-              {/* leaving this out for not because year and day are not required */}
-              {/* <CheckBox
-              containerStyle={{ backgroundColor: "white", borderColor: "white" }}
-              title='Age'
-              size={16}
-              checked={this.state.filters.DOB}
-              onPress={() => this.setState({
-                ...this.state, 
-                filters: {
-                  ...this.state.filters, 
-                  name: false,
-                  DOB: !this.state.filters.DOB,
-                  created: false,
-                  updated: false
-                }})}
-            /> */}
 
               <CheckBox
                 containerStyle={{
@@ -523,10 +407,7 @@ class FamilyConnectionsScreen extends Component {
         {/* Case List View Starts Here */}
         <View style={{ paddingBottom: 170 }}>
           <ScrollView>
-            {/* Displays text placeholder until cases load */}
-            {/* {console.log('isLOADING', this.props)} */}
             {this.props.isLoading ? (
-              // <Text style={styles.isLoading}> Loading Cases... </Text>
               <Loader />
             ) : (
                 SearchedCases.map((result, index) => (
@@ -543,67 +424,13 @@ class FamilyConnectionsScreen extends Component {
                     leftAvatar={{ source: { uri: result.picture } }}
                     to pDivider={true}
                     onPress={async () => {
-                      // this.setState({pk : result.pk});
-                      // this.setCaseVisible(true);
                       this.props.navigation.navigate('CaseView', {pk: result.pk, caseData: result})
                       
                     }}
-                    // Case badges for document value/count
-                    // badge={{
-                    //   value: result.count_documents,
-                    //   textStyle: {
-                    //     fontSize: 14,
-                    //     color: "white",
-                    //     backgroundColor: constants.highlightColor
-                    //   },
-                    //   containerStyle: { marginTop: -10 }
-                    // }}
                   />
                 ))
               )}
-
-            {/* CASE onPress MODAL */}
-            {/* <View
-              animationType="slide"
-              transparent={false}
-              visible={this.state.caseVisible}
-            >
-              <CaseViewScreen
-                pk={this.state.pk}
-                setModalVisible={this.setModalVisible}
-                setCaseVisible={() => {
-                  this.setCaseVisible(false);
-                }}
-              />
-            </View> */}
           </ScrollView>
-
-          {/* AddCase - onPress MODAL - leaving out for now */}
-          {/* <View>
-            <Modal
-              animationType="slide"
-              transparent={false}
-              visible={this.state.addCaseModalVisible}
-            >
-              <AddCaseScreen
-                setAddCaseModalVisible={this.setAddCaseModalVisible}
-                setAddCaseModalVisible={() => {
-                  this.setAddCaseModalVisible(false);
-                }}
-                addCaseModalVisible={this.state.addCaseModalVisible}
-              />
-              <ScrollView>
-                <View
-                  style={{
-                    marginVertical: 20,
-                    justifyContent: "center",
-                    alignSelf: "center"
-                  }}
-                >
-                </View>
-              </ScrollView>
-            </Modal>
-          </View> */}
         </View>
       </SafeAreaView>
     );
@@ -619,12 +446,6 @@ const styles = StyleSheet.create({
     width: '97%',
     backgroundColor: Platform.OS === "ios" ? "white" : "white",
   },
-  // addCaseButton: {
-  //   marginHorizontal: Platform.OS === "ios" ? 5 : 5,
-  //   width: Platform.OS === "ios" ? 200 : 200,
-  //   marginVertical: Platform.OS === "ios" ? 20 : 20,
-  //   maxHeight: Platform.OS === "ios" ? 40 : 40
-  // },
   filterButton: {
     width: Platform.OS === "ios" ? 70 : 70,
     marginVertical: Platform.OS === "ios" ? 20 : 20,
