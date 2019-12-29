8c89761cc3d047e689d062b333b152eb5173be01
Uzias Rivera <uziasrivera@gmail.com>
2019-12-29 14:15:06 -0800

    styled details view

--- src/screens/ConnectionsDetailsView.js
+++ src/screens/ConnectionsDetailsView.js
@@ -12,31 +12,59 @@ import {
 } from 'react-native';
 
 export default function ConnectionsDetailsView({details}){
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    rootView:{
+        display:'flex',
+        justifyContent:'flex-start',
+        width:'90%',
+        margin: 0
+    },
+    header:{
+        marginVertical: 20,
+        borderBottomWidth: .75,
+        borderBottomColor: 'black'
+    },
+    headerText:{
+        color: '#a1a1a1',
+        fontWeight: 'bold',
+    },
+    text:{
+        marginBottom: 25
+    }
+})
 return(
-    <View>
-        <Text>First Name {details.first_name}</Text>
-        <Text>Middle Name {details.middle_name}</Text>
-        <Text>Last Name {details.last_name}</Text>
-        <Text>Suffix {details.suffix}</Text>
-        <Text>Date of Birth {details.last_name}</Text>
-        <Text>Gender {details.gender}</Text>
-        <Text>Deceased {details.deceased? 'True': 'False'}</Text>
-        <Text>Residence {details.addresses.length?
-        details.addresses.map(address=>{address})
-        :null}</Text>
-        <Text>Telephone {details.telephones.length?
-        details.addresses.map(telephone=>{telephone})
-        :null}</Text>
-        <Text>Email {details.emails.length?
-        details.addresses.map(email=>{email})
-        :null}</Text>
-        <Text>Job Title {details.job_title}</Text>
-        <Text>Employer {details.employer}</Text>
-        <Text>Salary Range {details.salary_range}</Text>
-        <Text>Facebook {details.facebook}</Text>
-        <Text>LinkedIn {details.linkedin}</Text>
-        <Text>Twitter {details.twitter}</Text>
+    <View style={styles.rootView}>
+        <View style={styles.header}><Text style={styles.headerText}>INFORMATION</Text></View>
+        <View>
+            <Text style={styles.text}>First Name {details.first_name}</Text>
+            <Text style={styles.text}>Middle Name {details.middle_name}</Text>
+            <Text style={styles.text}>Last Name {details.last_name}</Text>
+            <Text style={styles.text}>Suffix {details.suffix}</Text>
+            <Text style={styles.text}>Date of Birth {details.birthday}</Text>
+            <Text style={styles.text}>Gender {details.gender}</Text>
+            <Text style={styles.text}>Deceased {details.deceased? 'Yes': 'No'}</Text>
+        </View>
+        <View style={styles.header}><Text style={styles.headerText}>CONTACT DETAILS</Text></View>
+        <View>
+            <Text style={styles.text}>Residence {details.addresses.length?
+            details.addresses.map(address=>{address})
+            :null}</Text>
+            <Text style={styles.text}>Telephone {details.telephones.length?
+            details.addresses.map(telephone=>{telephone})
+            :null}</Text>
+            <Text style={styles.text}>Email {details.emails.length?
+            details.addresses.map(email=>{email})
+            :null}</Text>
+            <Text style={styles.text}>Job Title {details.job_title}</Text>
+            <Text style={styles.text}>Employer {details.employer}</Text>
+            <Text style={styles.text}>Salary Range {details.salary_range}</Text>
+        </View>
+        <View style={styles.header}><Text style={styles.headerText}>SOCIAL MEDIA</Text></View>
+        <View>
+            <Text style={styles.text}>Facebook {details.facebook}</Text>
+            <Text style={styles.text}>LinkedIn {details.linkedin}</Text>
+            <Text style={styles.text}>Twitter {details.twitter}</Text>
+        </View>
     </View>
 )
 
